d4e42562e0f279ae8e032cf6d16cb2024e97ea54
mreiben <jason.eiben@gmail.com>
2017-01-01 19:20:06 +0000

    adding heroku

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello World!"); 
+    
+    //create object
+    var data = {
+        "ipaddress": req.headers['x-forwarded-for'],
+        "language": req.headers['accept-language'],
+        "software": req.headers['user-agent']
+    }
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
 });
 
-app.listen(8080, function(){
-    console.log("Listening on port 8080!");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, function() {
+    console.log('Listening on port %d!', server_port);
 });
